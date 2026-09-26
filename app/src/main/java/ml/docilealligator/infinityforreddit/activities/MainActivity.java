9fee232732175fee2b80560943debd6aacfbbd0c
karim <abdelkarimdjelalda@gmail.com>
2026-09-26 14:20:25 +0100

    Import ViewGroup for the navigation bar's inset handling

--- app/src/main/java/ml/docilealligator/infinityforreddit/activities/MainActivity.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/activities/MainActivity.java
@@ -27,6 +27,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
